469b83ea2acc745330f14a82a27a052e2cccf460
Tomas <tant836@aucklanduni.ac.nz>
2020-03-10 16:43:51 +1300

    removed comments relating to William's code review

--- server/src/main.py
+++ server/src/main.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python3
-# Requires:  CherryPy 18.0.1  (www.cherrypy.org)
-#            Python  (We use 3.5.x +)
-
 import os
 
 import cherrypy
@@ -10,14 +6,10 @@ import server
 
 import socket
 
-# The address we listen for connections on
 LISTEN_IP = "0.0.0.0"
 LISTEN_PORT = 1234
 
 def run_main_app():
-    #set up the config
-    #global LISTEN_IP
-    #LISTEN_IP = get_ip_address()
     conf = {
         '/': {
             'tools.staticdir.root': os.getcwd(),
@@ -29,28 +21,19 @@ def run_main_app():
             # The default session backend is in RAM. Other options are 'file',
         },
 
-        #configuration for the static assets directory
         '/static': {
             'tools.staticdir.on': True,
             'tools.staticdir.dir': 'static',
         },
-
-        #once a favicon is set up, the following code could be used to select it for cherrypy
-        #'/favicon.ico': {
-        #    'tools.staticfile.on': True,
-        #    'tools.staticfile.filename': os.getcwd() + '/static/favicon.ico',
-        #},
     }
 
     cherrypy.site = {
         'base_path': os.getcwd()
     }
-
-    # Create an instance of MainApp and tell Cherrypy to send all requests under / to it. (ie all of them)
+    
     cherrypy.tree.mount(server.ApiServer(), "/api/", conf)
     cherrypy.tree.mount(server.MainApp(), "/", conf)
 
-    # Tell cherrypy where to listen, and to turn autoreload on
     cherrypy.config.update({'server.socket_host': LISTEN_IP,
                             'server.socket_port': LISTEN_PORT,
                             'engine.autoreload.on': True,
@@ -60,12 +43,9 @@ def run_main_app():
     print("           Softeng 701 Server")
     print("========================================")
 
-    # Start the web server
     cherrypy.engine.start()
 
-    # And stop doing anything else. Let the web server take over.
     cherrypy.engine.block()
 
-#Run the function to start everything
 if __name__ == '__main__':
     run_main_app()
--- server/src/server.py
+++ server/src/server.py
@@ -8,13 +8,11 @@ SITE_ROOT = "../../frontend"
 
 class MainApp(object):
 
-	#CherryPy Configuration
     _cp_config = {'tools.encode.on': True,
                   'tools.encode.encoding': 'utf-8',
                   'tools.sessions.on' : 'True',
                  }
 
-	# If they try somewhere we don't know, catch it here and send them to the right place.
     @cherrypy.expose
     def default(self, *args, **kwargs):
         """The default page, given when we don't recognise where the request is for."""
@@ -22,7 +20,6 @@ class MainApp(object):
         cherrypy.response.status = "404server"
         return page
 
-    # PAGES (which return HTML that can be viewed in browser)
     @cherrypy.expose
     def index(self):
         page = open(SITE_ROOT + "/test/index.html", "r")
