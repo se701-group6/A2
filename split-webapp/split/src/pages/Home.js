9dfb83ecb746b7f42230af61cbc9e1e1d0143519
Vanessa Ciputra <vee.christella@gmail.com>
2020-03-12 17:49:28 +1300

    UI routing and page skeleton

---
+++ split-webapp/split/src/pages/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route, NavLink, HashRouter} from "react-router-dom"
+import Transactions from './Transactions';
+import Split from './Split';
+import '../App.css';
+
+function Home() {
+  return (
+    <HashRouter>
+    <div>
+      <div className="Sidebar">
+        <h1>Sp/it Home</h1>
+        <NavLink to="/login"><button>Sign Out</button></NavLink>
+        <ul className="">
+          <li><NavLink to="/home/transactions">Transactions</NavLink></li>
+          <li><NavLink to="/home/split">Split</NavLink></li>
+        </ul>
+        </div>
+        <div>
+          <Route path="/home/transactions" component={Transactions}/>
+          <Route path="/home/split" component={Split}/>
+
+        </div>
+      </div>
+    </HashRouter>
+  );
+}
+
+export default Home;
---
+++ split-webapp/split/src/pages/Transactions.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class Transactions extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Transactions</h2>
+                <p>LOREM IPSUM</p>
+            </div>
+        );
+    }
+}
+
+export default Transactions;
