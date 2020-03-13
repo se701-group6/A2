757be90132c6b625e0e1a4a751d95e95d5f57c5d
jake-good <jgoo479@aucklanduni.ac.nz>
2020-03-13 15:04:40 +1300

    Added material ui as dependancy and put in example button.

--- split-webapp/split/src/pages/Login.js
+++ split-webapp/split/src/pages/Login.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import { NavLink } from "react-router-dom"
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 
 function Login() {
   return (
-      <div className="App">
-        <header className="App-header">
+    <div className="App">
+      <header className="App-header">
         <h1>Sp/it Login</h1>
         <ul className="App-header">
-        <NavLink to="/home/transactions"><button>To Home</button></NavLink>
+          <NavLink to="/home/transactions">
+            <Button variant="contained" color="primary">
+              To Home
+            </Button>
+          </NavLink>
         </ul>
-        </header>
-      </div>
+      </header>
+    </div>
   );
 }
 
