c99e14c7889be5f01ea94e275b65b98a6b7d0eee
Ryan Lim <ryanlimenwei@gmail.com>
2020-03-15 22:18:48 +1300

    Removed the return statement when fetching data from the API

--- split-webapp/split/src/pages/Split.js
+++ split-webapp/split/src/pages/Split.js
@@ -1,99 +1,115 @@
-import React, {Component} from "react";
-import { Button, ListItem, ListItemText, ListItemIcon, Divider, TextField, List } from '@material-ui/core';
-
-
+import React, { Component } from "react";
+import {
+  Button,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Divider,
+  TextField,
+  List
+} from "@material-ui/core";
 
 class Split extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            transaction: {
-                users: [],
-                cost: 0,
-                payed: ""
-            }
-        }
-    }
+    this.state = {
+      transaction: {
+        users: [],
+        cost: 0,
+        payed: ""
+      }
+    };
+  }
 
-    addUser() {
-        let name = document.getElementById("name").value;
-        document.getElementById("name").value = "";
+  addUser() {
+    let name = document.getElementById("name").value;
+    document.getElementById("name").value = "";
 
-        this.setState(
-            {
-                transaction: {
-                    users: this.state.transaction.users.concat(name) 
-                }
-            }
-        )
-    }
+    this.setState({
+      transaction: {
+        users: this.state.transaction.users.concat(name)
+      }
+    });
+  }
 
-    split() {
-        let users = this.state.transaction.users;
-        let cost = document.getElementById("cost").value;
-        let perPersonCost = cost / users.length;
+  split() {
+    let users = this.state.transaction.users;
+    let cost = document.getElementById("cost").value;
+    let perPersonCost = cost / users.length;
 
-        let paymentArray = [];
+    let paymentArray = [];
 
-        users.forEach(user => {
-            paymentArray.push(
-                {
-                    name: user,
-                    amount: perPersonCost
-                }
-            )
-        })
-        
-        let bill = {
-            title: "Transaction title",
-            payer: "",
-            total: cost,
-            payments: paymentArray,
-        }
+    users.forEach(user => {
+      paymentArray.push({
+        name: user,
+        amount: perPersonCost
+      });
+    });
 
-        this.createBill(bill);
-    }
+    let bill = {
+      title: "Transaction title",
+      payer: "",
+      total: cost,
+      payments: paymentArray
+    };
 
-    createBill(data) {
-        return fetch('/api/bill_exec/create_bill', {
-            method: "POST",
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(res => {
-            return res;
-        }).catch(err => alert(err))
-    }
+    this.createBill(bill);
+  }
 
-    render() {
-        return (
-            <div>
-                <div className="Transactions">
-                    <div className="TransactionsTitle">
-                        <h1 className="TransactionsText">New Payment </h1>
-                        <TextField id="cost" placeholder="Amount Paid"></TextField>
-                    </div>
+  createBill(data) {
+    fetch("/api/bill_exec/create_bill", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        return res;
+      })
+      .catch(err => alert(err));
+  }
 
-                    <List component="nav" aria-label="main mailbox folders">
-                    {this.state.transaction && this.state.transaction.users.map((user, index) => 
-                        <ListItem button>
-                            <ListItemText primary={user} />
-                        </ListItem>
-                    )}
-                    </List>
-                    <Divider/>
-                    <TextField id="name" placeholder="Name" type="text"></TextField>
-                    <Button className="AddButton" onClick={() => this.addUser()} variant="contained">+</Button>
-                </div>
-                
-                <Button className="splitButton" variant="contained" color="primary" onClick={() => this.split()}>Split</Button> 
+  render() {
+    return (
+      <div>
+        <div className="Transactions">
+          <div className="TransactionsTitle">
+            <h1 className="TransactionsText">New Payment </h1>
+            <TextField id="cost" placeholder="Amount Paid"></TextField>
+          </div>
 
-            </div>
+          <List component="nav" aria-label="main mailbox folders">
+            {this.state.transaction &&
+              this.state.transaction.users.map((user, index) => (
+                <ListItem button>
+                  <ListItemText primary={user} />
+                </ListItem>
+              ))}
+          </List>
+          <Divider />
+          <TextField id="name" placeholder="Name" type="text"></TextField>
+          <Button
+            className="AddButton"
+            onClick={() => this.addUser()}
+            variant="contained"
+          >
+            +
+          </Button>
+        </div>
 
-        );
-    }
+        <Button
+          className="splitButton"
+          variant="contained"
+          color="primary"
+          onClick={() => this.split()}
+        >
+          Split
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default Split;
