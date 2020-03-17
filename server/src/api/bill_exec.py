fc1094dfc80ea2d15025f455ee7a462f9433887e
Iain <icam308@aucklanduni.ac.nz>
2020-03-17 18:36:19 +1300

    Updated add bill endpoint according to api

--- server/src/api/bill_exec.py
+++ server/src/api/bill_exec.py
@@ -24,10 +24,41 @@ class BillExecApi(object):
 
     @cherrypy.expose
     def create_bill(self):
-        """modifies transaction on database
-        Check database structure doc
+        """creates a bill and adds to 
+            the database accordingly
         
-        Arguments:
-            json -- check api doc for json format
+        JSON Arguments:
+            title -- user defined name of bill
+            payer -- name of person who fronted bill
+            total -- total bill amount
+            outstanding_payments -- list of outstanding payments
+                person -- name of payer
+                amount -- amount outstanding
+        JSON Returns:
+            success -- bool success of fail
+            message (optional) - error message if applicable
         """
-        return "3"
+        JSON_object = json.loads(cherrypy.request.body.read().decode('utf-8'))
+        title = JSON_object.get("title")
+        payer = JSON_object.get("payer")
+        total = JSON_object.get("total")
+        outstanding_payments = JSON_object.get("outstanding_payments")
+        
+        error_msg = ""
+        success = True
+        
+        try:
+            username = cherrypy.session["username"]
+            success = self.database.add_bill(payer,username,title,total,outstanding_payments)
+        except KeyError as e:
+            error_msg = "User not logged in"
+        except Exception as e:
+            print(e)
+            error_msg = "Unknown error occured"
+            success = False
+        finally:
+            response = {
+                'success' : success,
+                'message' : error_msg
+            }
+            return json.dumps(response)
