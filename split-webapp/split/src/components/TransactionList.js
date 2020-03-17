fe27fe91fc7c17a0b06a9ad9b4286f403b6e612d
Vanessa Ciputra <vee.christella@gmail.com>
2020-03-17 23:38:05 +1300

    Lint errors fixed

--- split-webapp/split/src/components/TransactionList.js
+++ split-webapp/split/src/components/TransactionList.js
@@ -18,87 +18,6 @@ class TransactionList extends React.Component {
     };
   }
 
-
-populateBills(bills) {
-  if (bills.length === 0) {
-    return;
-  } else {
-
-    console.log(bills);
-  return bills.map(bill => (
-    <ExpansionPanel className="Bills">
-      <ExpansionPanelSummary>
-      <div className="BillSummary">
-        <div className="BillTitle">
-          {bill.title}
-        </div>
-        <div className="BillAmount">
-          ${bill.total}
-        </div>
-        </div>
-      </ExpansionPanelSummary>
-      <ExpansionPanelDetails className="Payments">
-      <div className="PaymentsTitle">
-      <PaymentIcon className="PaymentHeaders"/>
-      </div>
-        {bill.payments.map(payment => {
-          const label = `${payment.from} owes ${payment.to} $${payment.amount}`;
-          return (
-            <div key={payment.id}>
-              <FormControlLabel
-                aria-label="Acknowledge"
-                onClick={() => this.markPaid({
-                  payment_id: payment.payment_id,
-                  is_paid: !payment.is_paid,
-                })}
-                onFocus={event => event.stopPropagation()}
-                control={<Checkbox checked={payment.is_paid} />}
-                label={label}
-              />
-            </div>
-          );
-        })}
-      </ExpansionPanelDetails>
-    </ExpansionPanel>
-  ));
-      }
-}
-
-setPaidStatus(paymentId, paid) {
-  this.setState({
-    bills: this.state.bills.map((bill) => {
-      return {
-        ...bill,
-        payments: bill.payments.map((payment) => { 
-        if (payment.payment_id === paymentId) {
-          return { 
-            ...payment,
-            is_paid: paid,
-          }
-        }
-        return payment;
-      }) }
-      
-    })
-  });
-}
-
-markPaid(data) {
-  fetch("/api/pay_exec/make_payment", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-    .then(res => {
-      // If "this" is not called in the createBill method, you may have to create the function outside of the class (es-lint rule)
-      this.setPaidStatus(data.payment_id, data.is_paid);
-      return res;
-    })
-    .catch(err => console.log(err));
-}
-
   componentDidMount() {
     //  actual address http://0.0.0.0:1234/api/bill_data/get_bill
     fetch("/api/bill_data/get_bills") // temp
@@ -117,13 +36,82 @@ markPaid(data) {
       });
   }
 
+  setPaidStatus(paymentId, paid) {
+    const { bills } = this.state;
+    this.setState({
+      bills: bills.map(bill => {
+        return {
+          ...bill,
+          payments: bill.payments.map(payment => {
+            if (payment.payment_id === paymentId) {
+              return {
+                ...payment,
+                is_paid: paid
+              };
+            }
+            return payment;
+          })
+        };
+      })
+    });
+  }
+
+  populateBills(bills) {
+    return bills.map(bill => (
+      <ExpansionPanel className="Bills">
+        <ExpansionPanelSummary>
+          <div className="BillSummary">
+            <div className="BillTitle">{bill.title}</div>
+            <div className="BillAmount">${bill.total}</div>
+          </div>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails className="Payments">
+          <div className="PaymentsTitle">
+            <PaymentIcon className="PaymentHeaders" />
+          </div>
+          {bill.payments.map(payment => {
+            const label = `${payment.from} owes ${payment.to} $${payment.amount}`;
+            return (
+              <div key={payment.id}>
+                <FormControlLabel
+                  aria-label="Acknowledge"
+                  onClick={() =>
+                    this.markPaid({
+                      payment_id: payment.payment_id,
+                      is_paid: !payment.is_paid
+                    })
+                  }
+                  onFocus={event => event.stopPropagation()}
+                  control={<Checkbox checked={payment.is_paid} />}
+                  label={label}
+                />
+              </div>
+            );
+          })}
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    ));
+  }
+
+  markPaid(data) {
+    fetch("/api/pay_exec/make_payment", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        // If "this" is not called in the createBill method, you may have to create the function outside of the class (es-lint rule)
+        this.setPaidStatus(data.payment_id, data.is_paid);
+        return res;
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     const { bills } = this.state;
-    return (
-      <div>
-        {this.populateBills(bills)}
-      </div>
-    );
+    return <div>{this.populateBills(bills)} </div>;
   }
 }
 
--- split-webapp/split/src/pages/Transactions.js
+++ split-webapp/split/src/pages/Transactions.js
@@ -1,20 +1,20 @@
 import React from "react";
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from "@material-ui/icons/Search";
+import { Divider } from "@material-ui/core";
 import TransactionList from "../components/TransactionList";
-import { Divider } from '@material-ui/core'
-import '../App.css';
+import "../App.css";
 
 function Transactions() {
-        return (
-            <div className="Transactions">
-            <div className="TransactionsTitle">
-                <h1 className="TransactionsText">Transactions </h1>
-                <SearchIcon className="SearchTransactions" />
-                </div>
-                <Divider />
-                <TransactionList />
-            </div>
-        );
+  return (
+    <div className="Transactions">
+      <div className="TransactionsTitle">
+        <h1 className="TransactionsText">Transactions </h1>
+        <SearchIcon className="SearchTransactions" />
+      </div>
+      <Divider />
+      <TransactionList />
+    </div>
+  );
 }
 
 export default Transactions;
