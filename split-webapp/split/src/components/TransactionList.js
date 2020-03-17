e846b8985d1f7edabd07a29f50a7e0bbfd6de5fb
Vanessa Ciputra <38397520+vee-christella@users.noreply.github.com>
2020-03-18 09:52:50 +1300

    Implemented payment API call to checkboxes + Styling of Transactions (#89)
    * Send POST request
    
    * Moved into class
    
    * Checkboxes now work!
    
    * Styling
    
    * Lint errors fixed
    
    * Added functionality for the case of 0 bills

--- split-webapp/split/src/components/TransactionList.js
+++ split-webapp/split/src/components/TransactionList.js
@@ -1,53 +1,13 @@
 import React from "react";
 import {
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  Divider,
   ExpansionPanel,
   ExpansionPanelSummary,
   ExpansionPanelDetails,
-  Typography,
   Checkbox,
   FormControlLabel
 } from "@material-ui/core";
 import PaymentIcon from "@material-ui/icons/Payment";
 import "../App.css";
-
-function ListItemLink(props) {
-  // Creates an error due to props spreading
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <ListItem button component="a" {...props} />;
-}
-
-function populateBills(bills) {
-  return bills.map(bill => (
-    <ExpansionPanel>
-      <ExpansionPanelSummary>
-        <Typography variant="h5">
-          {bill.title}, ${bill.total}
-        </Typography>
-      </ExpansionPanelSummary>
-      <ExpansionPanelDetails>
-        {bill.payments.map(payment => {
-          const label = `${payment.from} owes ${payment.to} $${payment.amount}`;
-          return (
-            <div key={payment.id}>
-              <FormControlLabel
-                aria-label="Acknowledge"
-                onClick={event => event.stopPropagation()}
-                onFocus={event => event.stopPropagation()}
-                control={<Checkbox checked={payment.is_paid} />}
-                label={label}
-              />
-            </div>
-          );
-        })}
-      </ExpansionPanelDetails>
-    </ExpansionPanel>
-  ));
-}
 
 class TransactionList extends React.Component {
   constructor(props) {
@@ -60,14 +20,14 @@ class TransactionList extends React.Component {
 
   componentDidMount() {
     //  actual address http://0.0.0.0:1234/api/bill_data/get_bill
-    fetch("https://jake-good.free.beeceptor.com/my/api/path") // temp
+    fetch("/api/bill_data/get_bills") // temp
       .then(res => {
         return res.json();
       })
       .then(data => {
         // make a mapping of bills to list items here and render it below
         this.setState({
-          bills: data
+          bills: data.bills
         });
       })
       .catch(err => {
@@ -75,51 +35,90 @@ class TransactionList extends React.Component {
       });
   }
 
+  setPaidStatus(paymentId, paid) {
+    const { bills } = this.state;
+    this.setState({
+      bills: bills.map(bill => {
+        return {
+          ...bill,
+          payments: bill.payments.map(payment => {
+            if (payment.payment_id === paymentId) {
+              return {
+                ...payment,
+                is_paid: paid
+              };
+            }
+            return payment;
+          })
+        };
+      })
+    });
+  }
+
+  populateBills(bills) {
+    const transaction = bills.map(bill => (
+      <ExpansionPanel className="Bills">
+        <ExpansionPanelSummary>
+          <div className="BillSummary">
+            <div className="BillTitle">{bill.title}</div>
+            <div className="BillAmount">${bill.total}</div>
+          </div>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails className="Payments">
+          <div className="PaymentsTitle">
+            <PaymentIcon className="PaymentHeaders" />
+          </div>
+          {bill.payments.map(payment => {
+            const label = `${payment.from} owes ${payment.to} $${payment.amount}`;
+            return (
+              <div key={payment.id}>
+                <FormControlLabel
+                  aria-label="Acknowledge"
+                  onClick={() =>
+                    this.markPaid({
+                      payment_id: payment.payment_id,
+                      is_paid: !payment.is_paid
+                    })
+                  }
+                  onFocus={event => event.stopPropagation()}
+                  control={<Checkbox checked={payment.is_paid} />}
+                  label={label}
+                />
+              </div>
+            );
+          })}
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    ));
+
+    if (transaction.length === 0) {
+      return (
+        <div className="NoTransactions">
+          You have no outstanding transactions.
+        </div>
+      );
+    }
+    return transaction;
+  }
+
+  markPaid(data) {
+    fetch("/api/pay_exec/make_payment", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        this.setPaidStatus(data.payment_id, data.is_paid);
+        return res;
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     const { bills } = this.state;
-    return (
-      <div>
-        {populateBills(bills)}
-        <List component="nav" aria-label="main mailbox folders">
-          <Divider />
-          <ListItem button>
-            <ListItemLink href="#simple-list" className="TransactionSummary">
-              <ListItemText primary="Ryan owes Vanessa $10" />
-              <ListItemIcon>
-                <PaymentIcon className="PaymentIcon" />
-              </ListItemIcon>
-            </ListItemLink>
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <ListItemLink href="#simple-list" className="TransactionSummary">
-              <ListItemText primary="Vanessa owes sean $4" />
-              <ListItemIcon>
-                <PaymentIcon className="PaymentIcon" />
-              </ListItemIcon>
-            </ListItemLink>
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <ListItemLink href="#simple-list" className="TransactionSummary">
-              <ListItemText primary="Ryan owes Vanessa a cat" />
-              <ListItemIcon>
-                <PaymentIcon className="PaymentIcon" />
-              </ListItemIcon>
-            </ListItemLink>
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <ListItemLink href="#simple-list" className="TransactionSummary">
-              <ListItemText primary="Sean owes Ryan an apology" />
-              <ListItemIcon>
-                <PaymentIcon className="PaymentIcon" />
-              </ListItemIcon>
-            </ListItemLink>
-          </ListItem>
-        </List>
-      </div>
-    );
+    return <div>{this.populateBills(bills)} </div>;
   }
 }
 
--- split-webapp/split/src/pages/Transactions.js
+++ split-webapp/split/src/pages/Transactions.js
@@ -1,18 +1,20 @@
 import React from "react";
-import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from "@material-ui/icons/Search";
+import { Divider } from "@material-ui/core";
 import TransactionList from "../components/TransactionList";
-import '../App.css';
+import "../App.css";
 
 function Transactions() {
-        return (
-            <div className="Transactions">
-            <div className="TransactionsTitle">
-                <h1 className="TransactionsText">Transactions </h1>
-                <SearchIcon className="SearchTransactions" />
-                </div>
-                <TransactionList />
-            </div>
-        );
+  return (
+    <div className="Transactions">
+      <div className="TransactionsTitle">
+        <h1 className="TransactionsText">Transactions </h1>
+        <SearchIcon className="SearchTransactions" />
+      </div>
+      <Divider />
+      <TransactionList />
+    </div>
+  );
 }
 
 export default Transactions;
