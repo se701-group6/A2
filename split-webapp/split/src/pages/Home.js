fcbda75104e59b0714ca6e091179e687ce9028c4
Andrew Hu <andrew.h318@hotmail.co.nz>
2020-04-23 21:17:55 +1200

    refactor login, sign up and home styles

--- split-webapp/split/src/pages/Home.js
+++ split-webapp/split/src/pages/Home.js
@@ -2,23 +2,25 @@ import React from "react";
 import { Route, NavLink, HashRouter } from "react-router-dom";
 import Transactions from "./Transactions";
 import Split from "./Split";
-import "../App.css";
 import NavList from "../components/NavList";
 import MenuBar from "../components/MenuBar";
+import styles from "./Home.module.css";
+
+console.log(styles);
 
 function Home() {
   return (
     <HashRouter>
-      <MenuBar className="AppBar" />
+      <MenuBar className={styles.appBar} />
       <div>
-        <div className="Sidebar">
-          <h1 className="SplitLogo">Sp/it</h1>
+        <div className={styles.sideBar}>
+          <h1 className={styles.splitLogo}>Sp/it</h1>
           <NavLink to="/">
             <button type="button">Sign Out</button>
           </NavLink>
           <NavList />
         </div>
-        <div className="HomeScreen">
+        <div className={styles.homeScreen}>
           <Route path="/home/transactions" component={Transactions} />
           <Route path="/home/split" component={Split} />
         </div>
