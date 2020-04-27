a1dabddb4832504f29c167304bed816a54cc4fa1
Ernest Wong <sudo@ernestwong.nz>
2020-04-27 20:18:14 +1200

    Polish split bills (#155)
    * Add remove user handler in split bill page
    
    * Add delete row button to split bill page
    
    * Implement drag-n-drop reordering of split bill people
    
    * Fix drag helper list style and add cursor for drag handle
    
    * Style Split-Bill page according to new design mockups
    
    - Open Sans added
    
    * Allow placeholder bill title to be submitted
    
    * Animate split-bill page when list changes
    
    * Remove toggling behaviour for split bill toggle buttons
    
    * Mark person's name required, and bill title not required
    
    * Always show at least 2 rows in split bill form
    
    * Implement split-bill inline error validation
    
    - Introduction of ErrorBubble components
    - Introduction of the errors object
    - Create a "fake" progress feedback delay
    
    * Don't reset payee when selected payer button is re-selected
    
    The "Payer" button should only do something if that person is a "payee".
    If the person is already a payer, pressing the "payer" button should not
    clear someone else's payee status.
    
    * Fix error bubble font weight
    
    Bubble should use normal italics, but only light italics were downloaded
    and the browser was forced to use artificial boldening.
    
    * Fix bug where deleting a payee row keeps the payee
    
    * Deleting last 2 rows should feel like "clearing" the rows
    
    Previously:
    Given only 2 rows in the list
    When the first row is deleted
    Then the second row moves up to the first row,
    and a new row is added to the end to the second row position.
    
    Now:
    Given only 2 rows in the list
    When the first row is deleted
    Then a new row is added back into the first row,
    and the second row is not affected.
    
    This simplifies the user's impression of the animation.
    
    * Refactor split-bill file structure to best practices
    
    I.e. dedicated folder for Split, and ErrorBubble
    
    * Prevent error bubble in row being overlapped by row below
    
    This bug happened in inconsistent cases.
    See comment in this commit.
    
    * Trim whitespace off split bill textfields
    
    * Fix fluid input not resizing with white spaces in input
    
    * Gracefully handle large inputs in split bill form
    
    A very long title should not push the total text field outside of the
    box.
    
    * Fix Safari focus stuck on previous textfield bug
    
    * Flatten FluidInput's fake span structure
    
    This is in attempt to simplify FluidInput's layout behaviour, hopefully
    making it easier to debug CSS problems due to misalignments and
    overflows.
    
    * Allow header error to overflow outside, and fix FluidInput alignment
    
    The cause of alignment problems was the padding we used to fix the
    clipped descenders in the text fields.
    
    The clipping of the descenders are now fixed by resetting the height and
    line-height attributes.
    
    * Reduce flip animation distortion due to border thickness
    
    Distortion was noticeable on Safari, and in some cases, other browsers
    too. To reproduce the distortion, create a list of people, then try
    deleting a row. As you delete a row, you may notice that the header text
    moves slightly even when it should remain still.

---
+++ split-webapp/split/src/components/ErrorBubble/index.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
+import styles from "./index.module.css";
+
+export const ErrorBubble = ({
+  className,
+  show,
+  message,
+  arrowLeft,
+  arrowTop,
+  center
+}) => {
+  const [shownMessage, setShownMessage] = useState(message);
+
+  useEffect(() => {
+    // Don't update shown message if it is empty.
+    // This is to keep messages shown when the bubble
+    // is fading out.
+    if (message) {
+      setShownMessage(message);
+    }
+  }, [message]);
+
+  return (
+    <span
+      className={[
+        show && message ? styles.show : styles.hidden,
+        center ? styles.center : "",
+        className
+      ].join(" ")}
+    >
+      {arrowLeft && <span className={styles.arrowLeft} />}
+      {arrowTop && <span className={styles.arrowTop} />}
+      {shownMessage}
+    </span>
+  );
+};
+
+ErrorBubble.propTypes = {
+  className: PropTypes.string,
+  show: PropTypes.bool.isRequired,
+  message: PropTypes.string,
+  arrowLeft: PropTypes.bool,
+  arrowTop: PropTypes.bool,
+  center: PropTypes.bool
+};
+
+ErrorBubble.defaultProps = {
+  className: "",
+  message: "",
+  arrowLeft: false,
+  arrowTop: false,
+  center: false
+};
+
+export const ErrorBubbleAnchor = ({ children, className }) => (
+  <span className={[styles.container, className].join(" ")}>{children}</span>
+);
+
+ErrorBubbleAnchor.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string
+};
+
+ErrorBubbleAnchor.defaultProps = {
+  className: ""
+};
---
+++ split-webapp/split/src/pages/Split/index.jsx
@@ -0,0 +1,413 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+
+import { Flipper, Flipped } from "react-flip-toolkit";
+import { v4 as uuidv4 } from "uuid";
+
+import Header from "./Header";
+import PeopleList from "./PeopleList";
+
+import styles from "./index.module.css";
+
+const DEFAULT_BILL_TITLE = "Untitled Bill";
+const MINIMUM_PEOPLE_COUNT = 2;
+
+const createBill = data => {
+  fetch("/api/bill_exec/create_bill", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(res => {
+      return res;
+    })
+    .catch(err => console.log(err));
+};
+
+const addUserTo = (transaction, insertionIndex) => {
+  const newId = uuidv4();
+  return {
+    ...transaction,
+    users: {
+      allIds: [
+        ...transaction.users.allIds.slice(0, insertionIndex),
+        newId,
+        ...transaction.users.allIds.slice(insertionIndex)
+      ],
+      byId: {
+        ...transaction.users.byId,
+        [newId]: {
+          name: ""
+        }
+      }
+    }
+  };
+};
+
+class Split extends Component {
+  constructor(props) {
+    super(props);
+
+    const initialUsers = {
+      allIds: [],
+      byId: {}
+    };
+
+    for (let i = 0; i < MINIMUM_PEOPLE_COUNT; i += 1) {
+      const userId = uuidv4();
+      initialUsers.allIds.push(userId);
+      initialUsers.byId[userId] = { name: "" };
+    }
+
+    this.state = {
+      transaction: {
+        title: "",
+        users: initialUsers,
+        cost: 0,
+        payed: null
+      },
+
+      // Do not start validating until after the first submission attempt.
+      showErrors: false,
+
+      // Delay submission by 200ms to give user a visual response.
+      // During this fake "processing" time, this flag is set.
+      submitAcknowledged: false
+    };
+  }
+
+  getNormalizedTransaction() {
+    const { transaction } = this.state;
+
+    const normalizedUsersById = {};
+
+    for (const userId of transaction.users.allIds) {
+      normalizedUsersById[userId] = {
+        ...transaction.users.byId[userId],
+        name: transaction.users.byId[userId].name.trim()
+      };
+    }
+
+    return {
+      ...transaction,
+      title: transaction.title.trim(),
+      users: {
+        ...transaction.users,
+        byId: normalizedUsersById
+      },
+    };
+  }
+
+  addUser = () => {
+    const { transaction } = this.state;
+
+    this.setState({
+      transaction: addUserTo(transaction, transaction.users.allIds.length)
+    });
+  };
+
+  removeUser = userId => {
+    const { transaction } = this.state;
+
+    const deleteIndex = transaction.users.allIds.indexOf(userId);
+
+    const updatedTransaction = {
+      ...transaction,
+      users: {
+        ...transaction.users,
+        allIds: transaction.users.allIds.filter(id => id !== userId)
+      }
+    };
+
+    delete updatedTransaction.users.byId[userId];
+
+    // Payed reference is no longer valid.
+    if (transaction.payed === userId) {
+      updatedTransaction.payed = null;
+    }
+
+    if (updatedTransaction.users.allIds.length >= MINIMUM_PEOPLE_COUNT) {
+      this.setState({
+        transaction: updatedTransaction
+      });
+    } else {
+      this.setState({
+        transaction: addUserTo(updatedTransaction, deleteIndex)
+      });
+    }
+  };
+
+  handlePeopleSwapOrder = (oldIndex, newIndex) => {
+    const { transaction } = this.state;
+
+    const newOrder = transaction.users.allIds.slice();
+    const item = newOrder.splice(oldIndex, 1)[0];
+    newOrder.splice(newIndex, 0, item);
+
+    this.setState({
+      transaction: {
+        ...transaction,
+        users: {
+          ...transaction.users,
+          allIds: newOrder
+        }
+      }
+    });
+  };
+
+  handleNameChange = (userId, newName) => {
+    const { transaction } = this.state;
+
+    this.setState({
+      transaction: {
+        ...transaction,
+        users: {
+          ...transaction.users,
+          byId: {
+            ...transaction.users.byId,
+            [userId]: {
+              ...transaction.users.byId[userId],
+              name: newName
+            }
+          }
+        }
+      }
+    });
+  };
+
+  handlePayeeChange = userId => {
+    const { transaction } = this.state;
+
+    this.setState({
+      transaction: { ...transaction, payed: userId }
+    });
+  };
+
+  handleTitleChange = event => {
+    const { transaction } = this.state;
+    const title = event.target.value;
+
+    this.setState({
+      transaction: { ...transaction, title }
+    });
+  };
+
+  handleTotalChange = (event, cost) => {
+    const { transaction } = this.state;
+
+    this.setState({
+      transaction: { ...transaction, cost }
+    });
+  };
+
+  handleInputBlur = event => {
+    // Workaround for Firefox:
+    //
+    // When text-overflow: ellipsis is set in Chrome, Chrome
+    // rewinds the input to the start when the input is blurred,
+    // so that the ellipsis can be shown.
+    // Firefox does not rewind to the start on blur, and so no
+    // ellipsis is shown even though the value is overflowing.
+    // Firefox supports two-value text-overflow syntax to
+    // work around this, but Chrome and other browsers don't support
+    // it yet.
+    //
+    // The solution is to emulate Chrome's rewinding behaviour on
+    // all browsers and use the single value syntax for text-overflow.
+    if (event.target.setSelectionRange && event.target.selectionStart > 0) {
+      event.target.setSelectionRange(0, 0);
+
+      // Workaround for Safari:
+      // In Safari, setSelectionRange puts focus back into the input,
+      // causing the focus system to become 'stuck' on the first
+      // textbox the user clicks on.
+      // De-focus from the text-box once more.
+      // Be careful with infinite recursion here.
+      event.target.blur();
+    }
+
+    this.setState({
+      transaction: this.getNormalizedTransaction()
+    });
+  };
+
+  split = async () => {
+    const transaction = this.getNormalizedTransaction();
+    const { history } = this.props;
+    const { users, cost } = transaction;
+    const perPersonCost = cost / users.allIds.length;
+    const title = transaction.title || DEFAULT_BILL_TITLE;
+
+    const paymentArray = [];
+
+    // Show visual feedback that the user's intention of submitting
+    // the form has been acknowledged.
+    this.setState({
+      showErrors: false,
+      submitAcknowledged: true
+    });
+    await new Promise(resolve => setTimeout(resolve, 200));
+
+    if (this.validate().hasError) {
+      // Begin live-validations after first failed submission.
+      this.setState({
+        showErrors: true,
+        submitAcknowledged: false
+      });
+      return;
+    }
+
+    for (const userId of users.allIds) {
+      if (userId !== transaction.payed) {
+        paymentArray.push({
+          person: users.byId[userId].name,
+          amount: perPersonCost
+        });
+      }
+    }
+
+    const bill = {
+      title,
+      payer: transaction.users.byId[transaction.payed].name,
+      total: cost,
+      outstanding_payments: paymentArray
+    };
+
+    createBill(bill);
+
+    history.push("/home/transactions");
+  };
+
+  validate() {
+    const transaction = this.getNormalizedTransaction();
+
+    const errors = {};
+
+    if (transaction.cost === 0) {
+      errors.cost = "Cannot split a zero amount";
+      errors.hasError = true;
+    }
+
+    if (transaction.cost < 0) {
+      errors.cost = "Cannot split a negative amount";
+      errors.hasError = true;
+    }
+
+    if (!transaction.users.byId[transaction.payed]) {
+      errors.payed = "Please choose a payee for this bill";
+      errors.hasError = true;
+    }
+
+    errors.users = {};
+    const uniqueNameSet = new Set();
+    for (const userId of transaction.users.allIds) {
+      const { name } = transaction.users.byId[userId];
+      if (!name) {
+        errors.users[userId] = "Please enter a name";
+        errors.hasError = true;
+      } else if (uniqueNameSet.has(name)) {
+        errors.users[userId] = "Same name can’t appear twice";
+        errors.hasError = true;
+      }
+      uniqueNameSet.add(name);
+    }
+
+    return errors;
+  }
+
+  render() {
+    const { transaction, showErrors, submitAcknowledged } = this.state;
+    const errors = this.validate();
+    const submissionFailed = showErrors && errors.hasError;
+
+    // We want the Flip toolkit to animate whenever the
+    // list of people changes, either by deletion, addition,
+    // or re-ordering.
+    const flipKey = transaction.users.allIds.join(" ");
+
+    let submitButtonText = "Split This Bill";
+    if (submissionFailed) {
+      submitButtonText = "We found some things to fix";
+    } else if (submitAcknowledged) {
+      submitButtonText = "Splitting...";
+    }
+
+    return (
+      <Flipper flipKey={flipKey}>
+        <div className={styles.page}>
+          <Flipped flipId="Split-card">
+            <div className={styles.card}>
+              <Flipped inverseFlipId="Split-card">
+                <div>
+                  <Header
+                    className={styles.header}
+                    title={transaction.title}
+                    titlePlaceholder={DEFAULT_BILL_TITLE}
+                    cost={transaction.cost}
+                    costError={errors.cost}
+                    showErrors={showErrors}
+                    onTitleChange={this.handleTitleChange}
+                    onCostChange={this.handleTotalChange}
+                    onInputBlur={this.handleInputBlur}
+                  />
+
+                  <PeopleList
+                    className={styles.peopleList}
+                    people={transaction.users}
+                    paidPersonId={transaction.payed}
+                    peopleError={errors.users}
+                    paidError={errors.payed}
+                    showErrors={showErrors}
+                    onPayeeChange={this.handlePayeeChange}
+                    onNameChange={this.handleNameChange}
+                    onRemovePerson={this.removeUser}
+                    onSwapOrder={this.handlePeopleSwapOrder}
+                    onInputBlur={this.handleInputBlur}
+                  />
+
+                  <Flipped flipId="Split-addPerson" translation>
+                    <Button
+                      onClick={this.addUser}
+                      fullWidth
+                      className={styles.addPersonButton}
+                      startIcon={<AddIcon />}
+                    >
+                      Add Person
+                    </Button>
+                  </Flipped>
+                </div>
+              </Flipped>
+            </div>
+          </Flipped>
+
+          <Flipped flipId="Split-splitButton" translation>
+            <Button
+              className={[
+                styles.splitButton,
+                submissionFailed ? styles.splitButtonError : "",
+                submitAcknowledged ? styles.splitButtonProgress : ""
+              ].join(" ")}
+              variant="contained"
+              fullWidth
+              disabled={submitAcknowledged}
+              onClick={this.split}
+            >
+              {submitButtonText}
+            </Button>
+          </Flipped>
+        </div>
+      </Flipper>
+    );
+  }
+}
+
+Split.propTypes = {
+  history: PropTypes.node.isRequired
+};
+
+export default Split;
