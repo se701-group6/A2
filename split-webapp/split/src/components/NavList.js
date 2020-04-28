19d3bcce481adcca7af1ea0d23ca7199b484a530
Andrew Hu <38745667+andrewh318@users.noreply.github.com>
2020-04-28 17:51:49 +1200

    Feature/css refactor (#157)
    * set cookie on backend after login
    
    * parse cookie sent from server and store in react context api to be used globally
    
    * set cookie in register API and process cookie in sign up on front end
    
    * refactor cookie setting into helper method
    
    * refactor nav list styles
    
    * refactor transaction list styles
    
    * refactor login, sign up and home styles
    
    * refactor transactions styhling
    
    * refactor login styles
    
    * refactor menu bar styles
    
    * use correct syntax for multiple class names
    
    * remove undefined login style
    
    * remove unused styles
    
    * refactor nav list styles
    
    * refactor transaction list styles
    
    * refactor login, sign up and home styles
    
    * refactor transactions styhling
    
    * refactor login styles
    
    * refactor menu bar styles
    
    * use correct syntax for multiple class names
    
    * remove undefined login style
    
    * remove unused styles
    
    * remove unused app css file

--- split-webapp/split/src/components/NavList.js
+++ split-webapp/split/src/components/NavList.js
@@ -2,7 +2,7 @@ import React from "react";
 import { List, ListItem, ListItemText, Divider } from "@material-ui/core";
 import PaymentIcon from "@material-ui/icons/Payment";
 import PeopleOutlineIcon from "@material-ui/icons/PeopleOutline";
-import "../App.css";
+import styles from "./NavList.module.css";
 
 function ListItemLink(props) {
   // eslint-disable-next-line react/jsx-props-no-spreading
@@ -14,22 +14,22 @@ export default function NavList() {
     <div>
       <List component="nav" aria-label="main mailbox folders">
         <Divider />
-        <ListItem button className="NavButton">
-          <PaymentIcon className="NavIcon" />
-          <ListItemLink href="#/home/transactions" className="NavButton">
+        <ListItem button className={styles.navButton}>
+          <PaymentIcon className={styles.navIcon} />
+          <ListItemLink href="#/home/transactions" className={styles.navButton}>
             <ListItemText
               primary="Transactions"
-              className="UnselectedNavText"
+              className={styles.unselectedNavText}
             />
           </ListItemLink>
         </ListItem>
         <Divider />
         <ListItem button>
-          <PeopleOutlineIcon className="NavIcon" />
+          <PeopleOutlineIcon className={styles.navIcon} />
           <ListItemLink href="#/home/split">
             <ListItemText
               primary="Split a Bill"
-              className="UnselectedNavText"
+              className={styles.unselectedNavText}
             />
           </ListItemLink>
         </ListItem>
