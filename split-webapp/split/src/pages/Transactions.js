5019e25217d10eb6b3b67c31656b7e95a2fd028c
Frank Wang Ma <frankwangma@gmail.com>
2020-04-30 14:06:32 +1200

    Added filters to display in the home page

--- split-webapp/split/src/pages/Transactions.js
+++ split-webapp/split/src/pages/Transactions.js
@@ -17,6 +17,38 @@ class Transactions extends Component {
     };
   }
 
+  displayFilters = () => {
+    const { sortField, sortOrder, isPaid, payer, payee } = this.state;
+    let sortFieldText = "";
+    let isPaidText = "";
+    if (sortField === "created_time") {
+      sortFieldText = "Created Time";
+    } else if (sortField === "amount") {
+      sortFieldText = "Total Amount";
+    } else {
+      sortFieldText = "Bill Name";
+    }
+
+    if (isPaid === "either") {
+      isPaidText = "Either";
+    } else if (isPaid === "paid") {
+      isPaidText = "Paid";
+    } else {
+      isPaidText = "Unpaid";
+    }
+
+    return (
+      <div>
+        <p>
+          <b>Sorting By:</b> {sortFieldText},{" "}
+          {sortOrder === "desc" ? "Descending" : "Ascending"}, <b>Paid:</b>{" "}
+          {isPaidText}, <b>Payee:</b> {payee === "" ? "Any" : payee},{" "}
+          <b>Payee:</b> {payer === "" ? "Any" : payer}
+        </p>
+      </div>
+    );
+  };
+
   changeFilters(
     sort_field = "",
     sort_order = "",
@@ -35,11 +67,15 @@ class Transactions extends Component {
 
   render() {
     const { sortField, sortOrder, isPaid, payer, payee } = this.state;
+
     return (
       <div className={styles.transactions}>
         <div className={styles.transactionsTitle}>
           <h1 className={styles.transactionsText}>Transactions </h1>
           <TransactionFilterButton changeFilters={this.changeFilters} />
+        </div>
+        <div className={styles.displayFilters}>
+          <h5>Current Filters:</h5> {this.displayFilters()}
         </div>
         <Divider />
         <TransactionList
